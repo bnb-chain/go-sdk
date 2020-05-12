c7f8b1847387fa00e710e080f303ed231ddb7add
HaoyangLiu <lhy.0318@163.com>
2020-05-12 19:55:05 +0800

    fix tendermint denpendency issue

--- types/tx/wire.go
+++ types/tx/wire.go
@@ -2,7 +2,7 @@ package tx
 
 import (
 	"github.com/tendermint/go-amino"
-	cryptoAmino "github.com/tendermint/tendermint/crypto/encoding/amino"
+	"github.com/tendermint/tendermint/crypto/encoding/amino"
 
 	"github.com/binance-chain/go-sdk/types/msg"
 )
