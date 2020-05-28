4349e8ed6c4ddaaf0ca017cdd8f5230281f7a9f1
Erheng Lu <erheng.lu@binance.com>
2020-05-28 14:00:26 +0800

    format pendingMatch json

--- common/types/depth.go
+++ common/types/depth.go
@@ -4,5 +4,5 @@ type MarketDepth struct {
 	Bids         [][]string `json:"bids"` // "bids": [ [ "0.0024", "10" ] ]
 	Asks         [][]string `json:"asks"` // "asks": [ [ "0.0024", "10" ] ]
 	Height       int64      `json:"height"`
-	PendingMatch bool
+	PendingMatch bool       `json:"pending_match"`
 }
