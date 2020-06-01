833a1fd2cdafe752a7edf557a17e255ae8e2eba9
erhenglu <42333959+erhenglu@users.noreply.github.com>
2020-06-01 17:55:01 +0800

    Revert "refactor according to changes in node"
    This reverts commit 5bb4eb32bc3271df67e22073a164fb1ea5aded03.

--- common/types/tokens.go
+++ common/types/tokens.go
@@ -19,7 +19,12 @@ type TokenBalance struct {
 
 // MiniToken definition
 type MiniToken struct {
-	Token
+	Name        string     `json:"name"`
+	Symbol      string     `json:"symbol"`
+	OrigSymbol  string     `json:"original_symbol"`
 	TokenType   int8       `json:"token_type"`
+	TotalSupply Fixed8     `json:"total_supply"`
+	Owner       AccAddress `json:"owner"`
+	Mintable    bool       `json:"mintable"`
 	TokenURI    string     `json:"token_uri"`
 }
